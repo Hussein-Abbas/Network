87fdb1d5d0ac04ec180346b28391e73723213749
Hussein-Abbas <hussein.ihsan.abbas@gmail.com>
2024-04-18 06:52:10 +0300

    add followers and following count and following fields

--- network/models.py
+++ network/models.py
@@ -3,4 +3,8 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followers_count = models.IntegerField(default=0)
+    following_count = models.IntegerField(default=0)
+
+    # People who the user is following them
+    following = models.ManyToManyField('self', symmetrical=False)

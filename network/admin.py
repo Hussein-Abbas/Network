937249c7cea45f0577130bc482d5fd43f9289384
Hussein-Abbas <hussein.ihsan.abbas@gmail.com>
2024-04-19 10:58:29 +0300

    Add Post model

--- network/admin.py
+++ network/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import *
 
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('poster', 'content', 'date', 'likes')
+
+
 admin.site.register(User)
+admin.site.register(Post, PostAdmin)
--- network/models.py
+++ network/models.py
@@ -1,7 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class User(AbstractUser):
-    # People who the user is following them
     following = models.ManyToManyField('self', related_name='followers', symmetrical=False, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.username
+
+
+class Post(models.Model):
+    poster = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    date = models.DateField(auto_now_add=True, editable=False)
+    likes = models.IntegerField(default=0, null=False, blank=False, validators=[MinValueValidator(0)])
+
+
+    def __str__(self):
+        return self.content
+

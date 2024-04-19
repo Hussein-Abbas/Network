b531ac87811e20339c5cc0fa79bd9b855654bcd3
Hussein-Abbas <hussein.ihsan.abbas@gmail.com>
2024-04-19 10:27:09 +0300

    Set one field in User model that is following because it is easy to get following count and followers count

--- network/models.py
+++ network/models.py
@@ -1,11 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.core.validators import MinValueValidator
 
 
 class User(AbstractUser):
-    followers_count = models.IntegerField(default=0, validators=[MinValueValidator(0)], null=False, blank=False)
-    following_count = models.IntegerField(default=0, validators=[MinValueValidator(0)], null=False, blank=False)
-
     # People who the user is following them
-    following = models.ManyToManyField('self', symmetrical=False, null=True, blank=True)
+    following = models.ManyToManyField('self', related_name='followers', symmetrical=False, null=True, blank=True)
